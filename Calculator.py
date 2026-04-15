894e14077d89e405fc0ef3d5ff004ca1e60d399e
Anaya_Abro <abroayaan8@gmail.com>
2026-04-15 22:56:02 +0500

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,67 @@
+print('Calculator🧮\n ')
+
+num1 = int(input('Enter your first number:')) 
+num2 = int(input('Enter your seconds number:'))
+operator = input('Choose your operator(+ - * % / //) ')
+
+if operator == '+':
+ print(num1 + num2)
+
+elif operator == '-':
+ print(num1 - num2)
+
+elif operator == '*':
+ print(num1 * num2)
+
+elif operator == '%':
+ print(num1 % num2)
+
+elif operator == '/':
+ print(num1 / num2)
+
+elif operator == '//':
+ print(num1 // num2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
